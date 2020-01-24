46939b3584111edd2fa11dc6e40af591b2774f58
kavelino <kleberth@icloud.com>
2020-01-24 13:41:32 -0500

    Presentacion

--- app/src/main/java/com/example/petplus/ui/mascota/MascotaViewModel.kt
+++ app/src/main/java/com/example/petplus/ui/mascota/MascotaViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class MascotaViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Rocky"
+        value = "Rocko"
     }
     val text: LiveData<String> = _text
 }
